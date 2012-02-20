0b6ea3a61ae5365cd58a066006072860f16c4fe3
jackson_6763@hotmail.com <Jackson@.(none)>
2012-02-20 15:40:29 -0800

    Adding strings & XML files for buttons

--- MapGenerator/src/mapgen/hex/GenMenu.java
+++ MapGenerator/src/mapgen/hex/GenMenu.java
@@ -1,12 +1,23 @@
 package mapgen.hex;
 
+import mapgen.hex.R;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import android.content.Intent;
 
 public class GenMenu extends MainMenu {
-	
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gen);
+        
+        final Button button = (Button) findViewById(R.id.Back_button); 
+        button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {return;}
+		}); 
     }
+    //public void onBackPressed() {return;}
 }
--- MapGenerator/src/mapgen/hex/MainMenu.java
+++ MapGenerator/src/mapgen/hex/MainMenu.java
@@ -1,27 +1,26 @@
 package mapgen.hex;
 
+import mapgen.hex.R;
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.view.View;
+import android.widget.*;
 import android.content.Intent;
 
 public class MainMenu extends Activity {
-    	
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        final Button button = (Button) findViewById(R.id.generate);
-        button.setOnClickListener(new View.OnClickListener(){
-        	public void onClick(View v) {
-        		Intent Generate = new Intent(MainMenu.this, GenMenu.class);
-        		MainMenu.this.startActivity(Generate);
-        	}
-        });
-    
-    
-        
+        final Button button = (Button) findViewById(R.id.New_button); 
+        button.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent Generate = new Intent(MainMenu.this, GenMenu.class);
+				MainMenu.this.startActivity(Generate);
+			}
+		});   
     }
+    //public void onBackPressed() {return;}
 }
